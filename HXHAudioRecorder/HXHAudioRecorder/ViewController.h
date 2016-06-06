6b9adf8b7a818caad9a2a57346f3026f264c0a53
张强 <zq252125@163.com>
2016-06-06 17:38:46 +0800

    first init

---
+++ HXHAudioRecorder/HXHAudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXHAudioRecorder
+//
+//  Created by 张强 on 16/6/6.
+//  Copyright © 2016年 ColorPen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
